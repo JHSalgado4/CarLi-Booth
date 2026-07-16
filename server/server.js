68d5ef99e4236cee7e8428c26bef4620b4adf5fe
Julia Salgado <salgado.julia.hernandez@gmail.com>
2026-07-17 00:07:18 +0800

    Add backend start script for deployment

--- server/server.js
+++ server/server.js
@@ -27,5 +27,5 @@ app.use("/api/photos", photoRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
